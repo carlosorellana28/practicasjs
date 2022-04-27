30b42775fb1639b30bdd1c5ea9490b66f514bbc1
Carlos orellana <eduardoorellanarapalo@gmail.com>
2022-04-26 20:40:59 -0600

    expreciones Regulares

---
+++ exprecionesRegulares/app.js
@@ -0,0 +1,14 @@
+/* CREAR EXPRECIONES REGULARES */
+
+/* las expreciones regulares siempre se ponen entre barras inclinadas
+
+******** si lo que queremos es buscar caracteres especificos los encerramos entre parentesis
+******* si solo es la palabra la colocamos completa dentro de las barras
+*******si queremos coisidir con una palabre u otra lo colocamos con una barra vertical 
+Ejemplo: /Carlos|Carl/i
+******la letra i  significa que no distinga entre mayusculas y minusculas
+******** la letra g significa que busque globalmente el contenido entre una cadena de texto
+
+*/
+
+const regExp = 
